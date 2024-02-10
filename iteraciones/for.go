dae192ad2faf52062fdeb0a7cbe37f52d88701c6
Johan <johan@bluecell.co>
2024-02-10 09:15:56 -0500

    Siguiente commit

--- iteraciones/for.go
+++ iteraciones/for.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+/*
+* Solo podemos usar for para iterar
+* para simular un while se usa
+* for {
+*
+*}
+* usar break para romper el ciclo, se usa continue para evitar algo
+ */
+
 func Iterar() {
 	i := 0
 	for i < 10 {
