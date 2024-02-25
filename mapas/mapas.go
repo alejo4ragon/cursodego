6baae461216fe0c506e8b2c96c4569bebe5c7a47
Johan <johan@bluecell.co>
2024-02-25 18:48:03 -0500

    Siguiente commit

--- mapas/mapas.go
+++ mapas/mapas.go
@@ -4,5 +4,36 @@ import "fmt"
 
 func MostrarMapas() {
 	paises := make(map[string]string)
-	fmt.Println(paises)
+
+	paises["Mexico"] = "Ciudad de méxico"
+	paises["Argentina"] = "Buenos Aires"
+	paises["Colombia"] = "Bogotá"
+	paises["España"] = "Madrid"
+	paises["Alemania"] = "Berlín"
+
+	// fmt.Println(paises)
+	// fmt.Println(paises["Colombia"])
+
+	campeonato := map[string]int{
+		"Barcelona":    39,
+		"Real Madrid":  38,
+		"Chivas":       37,
+		"Boca Juniors": 30,
+	}
+
+	// fmt.Println(campeonato)
+
+	// for equipo, puntaje := range campeonato {
+	// 	fmt.Printf("Equipo %s, tiene un puntaje de %d \n", equipo, puntaje)
+	// }
+
+	delete(campeonato, "Chivas")
+	fmt.Println(campeonato)
+
+	campeonato["Juventus"] = 80
+
+	puntaje, existe := campeonato["Juventus"]
+	fmt.Printf("El puntaje capturado es %d y el equipo existe = %t \n", puntaje, existe)
+	fmt.Println(campeonato)
+
 }
