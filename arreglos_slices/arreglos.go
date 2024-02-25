33280a0b61749c89102feb30985f061c28e96c27
Johan <johan@bluecell.co>
2024-02-25 12:11:18 -0500

    Siguiente commit

--- arreglos_slices/arreglos.go
+++ arreglos_slices/arreglos.go
@@ -3,8 +3,11 @@ package arreglos_slices
 import "fmt"
 
 var tabla [10]int //Arreglo de 10 elementos
+var tabla2 [10]int = [10]int{1, 2, 3, 4}
+
 func MuestroArreglos() {
 	tabla[7] = 33
 	tabla[2] = 54
 	fmt.Println(tabla)
+	fmt.Println(tabla2)
 }
